993af64ec29c1d243ec5c14385e613faa0aea3a4
nobel <nobel2073@gmail.com>
2024-06-14 22:03:12 +0545

    add background music

--- game.py
+++ game.py
@@ -3,6 +3,8 @@ from OpenGL.GL import *
 import glfw
 from enum import Enum
 import numpy as np
+import threading
+import subprocess
 from text import Font
 from constants import *
 import random
@@ -19,7 +21,7 @@ class Game:
         self.mouse_pressed = False
         self.last_gravity_update = 0
         self.font = Font('8_bit_wonder.ttf', 24)
-        self.menu_items = [("Start", (WIDTH//2 - 100, HEIGHT//2)), ("EXIT", (WIDTH//2 - 100, HEIGHT//2 - 60))]
+        self.menu_items = [("Start", (WIDTH // 2 - 100, HEIGHT // 2)), ("EXIT", (WIDTH // 2 - 100, HEIGHT // 2 - 60)),("SAND SIMULATION", (750,650))]
         self.game_items = [("Sand", (50, OPTIONS_Y)), ("Solid", (200, OPTIONS_Y)), ("Erase", (400, OPTIONS_Y)), ("TNT", (600, OPTIONS_Y))]
         self.in_menu = True
         self.current_tool = None
@@ -46,7 +48,7 @@ class Game:
             CellKind.EMPTY: BG_COLOR,
             CellKind.SAND: SAND_COLOR,
             CellKind.SOLID: SOLID_COLOR,
-            CellKind.TNT: TNT_COLOR 
+            CellKind.TNT: TNT_COLOR
         }.get(cell_kind, BG_COLOR)
 
     def update_cell(self, xpos, ypos, button):
@@ -64,7 +66,7 @@ class Game:
     def apply_gravity(self):
         for y in range(1, GRID_HEIGHT):
             for x in range(GRID_WIDTH):
-                if self.cells[y][x] == CellKind.SAND :
+                if self.cells[y][x] == CellKind.SAND:
                     if self.cells[y - 1][x] == CellKind.EMPTY:
                         self.cells[y][x] = CellKind.EMPTY
                         self.cells[y - 1][x] = CellKind.SAND
@@ -75,22 +77,21 @@ class Game:
                         self.cells[y][x] = CellKind.EMPTY
                         self.cells[y - 1][x - 1] = CellKind.SAND
                 elif self.cells[y][x] == CellKind.TNT:
-                    if self.cells[y - 1][x] == CellKind.EMPTY:   
+                    if self.cells[y - 1][x] == CellKind.EMPTY:
                         if y <= 1:
                             self.cells[y][x] = CellKind.EMPTY
                             return
                         self.cells[y][x] = CellKind.EMPTY
                         self.cells[y - 1][x] = CellKind.TNT
-                    elif self.cells[y-1][x] == CellKind.SAND or self.cells[y-1][x] == CellKind.SOLID:
+                    elif self.cells[y - 1][x] == CellKind.SAND or self.cells[y - 1][x] == CellKind.SOLID:
                         self.cells[y][x] = CellKind.EMPTY
-                        self.flood_fill(x,y-1)
-                    elif self.cells[y-1][x-1] == CellKind.SAND or self.cells[y][x-1] == CellKind.SOLID:
+                        self.flood_fill(x, y - 1)
+                    elif self.cells[y - 1][x - 1] == CellKind.SAND or self.cells[y][x - 1] == CellKind.SOLID:
                         self.cells[y][x] = CellKind.EMPTY
-                        self.flood_fill(x-1,y-1)
-                    elif self.cells[y-1][x+1] == CellKind.SAND or self.cells[y][x+1] == CellKind.SOLID:
+                        self.flood_fill(x - 1, y - 1)
+                    elif self.cells[y - 1][x + 1] == CellKind.SAND or self.cells[y][x + 1] == CellKind.SOLID:
                         self.cells[y][x] = CellKind.EMPTY
-                        self.flood_fill(x+1,y-1)
-                    
+                        self.flood_fill(x + 1, y - 1)
 
     def flood_fill(self, x, y):
         target = self.cells[y][x]
@@ -98,15 +99,14 @@ class Game:
         stack = [(x, y)]
         while stack and iterations:
             cx, cy = stack.pop()
-            iterations-=1
+            iterations -= 1
             if 0 <= cx < GRID_WIDTH and 0 <= cy < GRID_HEIGHT and self.cells[cy][cx] == target:
                 self.cells[cy][cx] = CellKind.EMPTY
-                stack.extend([(cx + 1, cy), (cx - 1, cy), 
+                stack.extend([(cx + 1, cy), (cx - 1, cy),
                               (cx, cy + 1), (cx, cy - 1),
                               (cx + 1, cy + 1), (cx - 1, cy - 1),
                               (cx + 1, cy - 1), (cx - 1, cy + 1)])
-                
-                    
+
     def render_menu(self):
         for item, position in self.menu_items:
             self.font.render_text(position[0], position[1], item, gap=2, color=MENU_COLOR)
@@ -120,7 +120,7 @@ class Game:
         ypos -= 24
         for item, position in self.menu_items:
             text_width = len(item) * 24
-            if position[0]-24 <= xpos <= (position[0] + text_width) and position[1]-24 <= ypos <= (position[1] + 24):
+            if position[0] - 24 <= xpos <= (position[0] + text_width) and position[1] - 24 <= ypos <= (position[1] + 24):
                 if item == "Start":
                     self.in_menu = False
                 elif item == "EXIT":
@@ -131,7 +131,7 @@ class Game:
         ypos -= 24
         for item, position in self.game_items:
             text_width = len(item) * 24
-            if position[0] <= xpos <= (position[0] + text_width) and position[1]-24 <= ypos <= (position[1] + 24):
+            if position[0] <= xpos <= (position[0] + text_width) and position[1] - 24 <= ypos <= (position[1] + 24):
                 if item == "Sand":
                     self.current_tool = CellKind.SAND
                 elif item == "Solid":
@@ -159,9 +159,15 @@ def cursor_position_callback(window, xpos, ypos):
     if game.mouse_pressed:
         game.update_cell(xpos, ypos, glfw.get_mouse_button(window, glfw.MOUSE_BUTTON_LEFT))
 
+def play_music(file):
+    return subprocess.Popen(["ffplay", "-nodisp","-loop", "-1", "-autoexit","-af", "volume=0.15,afade=type=in:start_time=0:duration=10", file])
+
+
 def main():
     global game, window
     game = Game()
+
+    music_process = play_music("jam_jam.mp3")
 
     if not glfw.init():
         raise Exception("GLFW initialization failed")
@@ -169,6 +175,7 @@ def main():
     window = glfw.create_window(WIDTH, HEIGHT, "Cellular Automaton", None, None)
     if not window:
         glfw.terminate()
+        music_process.terminate()  # Ensure music process is terminated
         raise Exception("Window creation failed")
 
     glfw.make_context_current(window)
@@ -203,6 +210,7 @@ def main():
         glfw.poll_events()
 
     glfw.terminate()
+    music_process.terminate()  # Terminate music process when exiting
 
 if __name__ == "__main__":
     main()
